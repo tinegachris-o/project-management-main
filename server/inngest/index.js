b4d367da706bad6da499da2e72c4629d773e369e
Tinega Chris <tinegachris797@gmail.com>
2025-12-01 15:34:08 +0300

    ingest function

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -52,9 +52,78 @@ const syncUserUpdation=inngest.createFunction(
         }})
     }
 )
+//inest functon to sync workspace creation
+
+const syncWorkspaceCreation=inngest.createFunction(
+    {id:'sync-workspace-from-clerk'},
+    {event:'clerk/workspace.created'},
+    async({event})=>{
+        const {data}=event
+        await prisma.workspace.create({
+            data:{
+            id:data.id,
+            name:data.name,
+            ownerId:data.created_by ,
+            slug:data.slug,
+            image:data.image_url,
+        }})
+        // Add creator as admin member of the workspace
+        await prisma.workspaceMember.create({
+            data:{
+                userId:data.created_by,
+                workspaceId:data.id,
+                role:'ADMIN'
+            }
+        })
+    }
+)
+//inngest function to sync workspace updation
+const syncWorkspaceUpdation=inngest.createFunction(
+    {id:'update-workspace-from-clerk'},
+    {event:'clerk/workspace.updated'},
+    async({event})=>{
+        const {data}=event
+        await prisma.workspace.update({
+            where:{id:data.id},
+            data:{
+            name:data.name,
+            slug:data.slug,
+            image:data.image_url,
+        }})
+    }
+)
+//inngest function to sync workspace deletion
+const syncWorkspaceDeletion=inngest.createFunction(
+    {id:'delete-workspace-from-clerk'},
+    {event:'clerk/workspace.deleted'},
+    async({event})=>{
+        const {data}=event
+        await prisma.workspace.deleteMany({
+            where:{id:data.id}
+        })
+    }
+)
+//sync worlspaceMember creation 
+const syncWorkspaceMemberCreation=inngest.createFunction(
+    {id:'sync-workspace-member-from-clerk'},
+    {event:'clerk/workspaceInvitation.accepted'}, 
+    async({event})=>{
+        const {data}=event
+        await prisma.workspaceMember.create({
+            data:{
+            userId:data.user_id,
+            workspaceId:data.workspace_id,
+            role:String(data.role).toUpperCase(),
+        }})
+    }
+)
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserDeletion,
-    syncUserUpdation
+    syncUserUpdation,
+    syncWorkspaceCreation,
+    syncWorkspaceUpdation,
+    syncWorkspaceDeletion,
+    syncWorkspaceMemberCreation
 ];
